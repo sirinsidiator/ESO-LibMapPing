2ba2995f3a2071701dd49bafd3e31cb2a71007d2
votan <votan@ogitix.de>
2020-01-12 11:18:16 +0100

    Make LibStub optional: - Optional depend on most common sources of LibStub, but still work, if it is missing. - Do not bundled LibStub anymore.

--- src/LibMapPing.lua
+++ src/LibMapPing.lua
@@ -1,9 +1,10 @@
 local LIB_IDENTIFIER = "LibMapPing"
-local lib = LibStub:NewLibrary(LIB_IDENTIFIER, 999) -- only for test purposes. releases will get a smaller number
+local lib = LibStub and LibStub:NewLibrary(LIB_IDENTIFIER, 999) -- only for test purposes. releases will get a smaller number
 
-if not lib then
+if LibStub and not lib then
     return -- already loaded and no upgrade necessary
 end
+lib = lib or {}
 
 local logger = LibDebugLogger.Create(LIB_IDENTIFIER)
 
