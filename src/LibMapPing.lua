7cd3a3686902772920bd226e186569cb845171f4
sirinsidiator <insidiator@cmos.at>
2020-02-26 23:18:22 +0100

    fixed copy pasta

--- src/LibMapPing.lua
+++ src/LibMapPing.lua
@@ -1,5 +1,5 @@
 local LIB_IDENTIFIER = "LibMapPing"
-local lib = LibStub and LibStub:NewLibrary(LIB_NAME, 999) -- only for test purposes. releases will get a smaller number
+local lib = LibStub and LibStub:NewLibrary(LIB_IDENTIFIER, 999) -- only for test purposes. releases will get a smaller number
 
 if LibStub and not lib then
     return -- already loaded and no upgrade necessary
