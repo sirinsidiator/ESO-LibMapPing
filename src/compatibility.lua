774875483658f090ed2d2784ce9837306ef6c90c
sirinsidiator <insidiator@cmos.at>
2025-04-30 18:02:31 +0200

    updated license information

--- src/compatibility.lua
+++ src/compatibility.lua
@@ -1,6 +1,6 @@
--- LibMapPing & its files © sirinsidiator                       --
--- Distributed under The Artistic License 2.0 (see LICENSE)     --
-------------------------------------------------------------------
+-- SPDX-FileCopyrightText: 2025 sirinsidiator
+--
+-- SPDX-License-Identifier: Artistic-2.0
 
 local libv2 = LibMapPing2
 
